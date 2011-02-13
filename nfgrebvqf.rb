93679308ceae9554582984510ec534d042355817
Kevin Alford <zeroeth@pixelflow.org>
2011-02-13 15:15:28 -0500

    - organized media into folders - notes out of main file

--- nfgrebvqf.rb
+++ nfgrebvqf.rb
@@ -9,24 +9,6 @@ puts "*** configuring baby seals"
 
 require 'level'
 
-# NOTE trait/state ideas
-# - a vector.. or discrete physics trait?
-#   * velocity/acceleration for angle
-#   * propel/repel other objects velocity/acceleration
-#   * helpers for angle_to.. objects/mouse/center
-# - confirmation popup state
-# - in game ruby debug console
-# NOTE game features
-# - bullet repeat / charge beam
-# - other weapons.. laser (ray through multiple items), seeking missiles
-# - energy? (some kind of trade off for firing/shields)
-# - accuracy
-# - game states
-# - high scores
-# - lives
-# - charge beam
-# - other weapon drops, with limited ammo
-
 # this is a bug in chingu, i shouldnt need it.
 
 class Game < Chingu::Window
